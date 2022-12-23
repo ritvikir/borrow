c84a0a41534743438c489b63f800b2402bec0ead
Bian Lee <bian@Jongwans-MacBook-Pro.local>
2022-12-23 14:02:24 -0800

    some updates

---
+++ components/Hero.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Link from "next/link";
+
+import data from "../data/data.json"
+export default class Hero extends React.Component{
+
+    constructor() {
+        super();
+        this.state = {
+
+        } 
+        this.handleClick = this.handleClick.bind(this);
+
+    }
+
+
+    handleClick(e) {
+        console.log(e.target.id);
+    }
+
+    render() {
+        return(
+            <div>
+                <section className="bg-blend-darken bg-cover bg-hero-pattern">
+                    <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+                    <div class="grid grid-cols-2 gap-4 h-80 md:grid-cols-4 ">
+                        {data.map((item) => {
+                             return <><div onClick={(e) => this.handleClick(e)} id={item.id} class="bg-rose-400 text-white py-5 hover:bg-rose-500" >{item.name}<br/>{item.cost}</div></>
+                        })}
+                    </div>
+    
+                    </div>
+                </section>
+            </div>
+        )
+    }
+
+}
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ import { FaGoogle } from "react-icons/fa";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
+import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
+
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
@@ -96,90 +99,14 @@ function SignOut() {
   }
 
   return (
-    auth.currentUser && (
-      <div className="container">
-        <Disclosure as="nav" className=" my-5 rounded-full bg-pink-400">
-          {({ open }) => (
-            <>
-              <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                <div className="relative flex items-center justify-between h-16">
-                  <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                    {/* Mobile menu button*/}
-                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XIcon className="block h-6 w-6" aria-hidden="true" />
-                      ) : (
-                        <MenuIcon
-                          className="block h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      )}
-                    </Disclosure.Button>
-                  </div>
-                  <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                    <div className="flex-shrink-0 flex items-center">
-                      {/* <img
-                        className="block lg:hidden h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                        alt="Workflow"
-                      />
-                      <img
-                        className="hidden lg:block h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                        alt="Workflow"
-                      /> */}
-                      <h1 className="font-bold text-white text-2xl">Borrow</h1>
-                    </div>
-                  </div>
-                  <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    {/* Profile dropdown */}
-                    <Menu as="div" className="ml-3 relative">
-                      <div>
-                        <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                          <span className="sr-only">Open user menu</span>
-                          <img
-                            className="h-10 w-10 rounded-full"
-                            src={auth.currentUser.photoURL}
-                            alt=""
-                          />
-                        </Menu.Button>
-                      </div>
-                      <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                      >
-                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          <Menu.Item>
-                            {({ active }) => (
-                              <a
-                                href="#"
-                                className={classNames(
-                                  active ? "bg-gray-100" : "",
-                                  "block px-4 py-2 text-sm text-gray-700"
-                                )}
-                                onClick={() => auth.signOut()}
-                              >
-                                Sign out
-                              </a>
-                            )}
-                          </Menu.Item>
-                        </Menu.Items>
-                      </Transition>
-                    </Menu>
-                  </div>
-                </div>
-              </div>
-            </>
-          )}
-        </Disclosure>
-      </div>
-    )
+
+    <>
+    <Navbar/>
+    <Hero/>
+    </>
+
+
+  
   );
 }
 

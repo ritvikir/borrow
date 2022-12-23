591dabc462f66b36171f53cd5208f4acc66acb0e
Bian Lee <bianlee2004@gmail.com>
2022-12-23 15:47:11 -0800

    JSON

--- components/Hero.js
+++ components/Hero.js
@@ -25,7 +25,12 @@ export default class Hero extends React.Component{
                     <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
                     <div class="grid grid-cols-2 gap-4 h-80 md:grid-cols-4 ">
                         {data.map((item) => {
-                             return <><div onClick={(e) => this.handleClick(e)} id={item.id} class="bg-rose-400 text-white py-5 hover:bg-rose-500" >{item.name}<br/>{item.cost}</div></>
+                             return <><div onClick={(e) => this.handleClick(e)} id={item.id} class="bg-rose-400 text-white py-5 px-5 hover:bg-rose-500 text-left">
+                                 <span className="font-bold">{item.name}</span> • {item.rating}/5
+                                 <br/>Lender: {item.lender}
+                                 <br/>Duration: {item.duration == 1 ? <>1 day</> : <>Up to {item.duration} days</>}
+                                 <br/>Base Cost: {item.cost}
+                                 </div></>
                         })}
                     </div>
     

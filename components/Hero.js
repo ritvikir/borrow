d3fd13177ce37fe31c31b254f2611e574a11b84e
Andrews'sSavagery <40343154+andrewscoding2018@users.noreply.github.com>
2022-12-25 14:08:07 -0800

    Stars in the hero

--- components/Hero.js
+++ components/Hero.js
@@ -18,6 +18,15 @@ export default class Hero extends React.Component{
         console.log(e.target.id);
     }
 
+    showStars(item){
+        const starTotal = 5;
+        const starPercentage = (item.rating / 5) * 100;
+        const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
+        document.querySelector('.stars-inner').style.width = starPercentageRounded;
+        //document.querySelector(`.${item.rating} .stars-inner`).style.width = starPercentageRounded;
+    }
+
+
     render() {
         return(
             <div>
@@ -25,8 +34,12 @@ export default class Hero extends React.Component{
                     <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
                     <div class="grid grid-cols-2 gap-4 h-80 md:grid-cols-4 ">
                         {data.map((item) => {
+                            this.showStars(item)
                              return <><div onClick={(e) => this.handleClick(e)} id={item.id} class="bg-rose-400 text-white py-5 px-5 hover:bg-rose-500 text-left">
-                                 <span className="font-bold">{item.name}</span> • {item.rating}/5
+                                 <span className="font-bold">{item.name}</span>
+                                 <div class = "stars-outer">
+                                     <div class = "stars-inner"></div>
+                                 </div>
                                  <br/>Lender: {item.lender}
                                  <br/>Duration: {item.duration == 1 ? <>1 day</> : <>Up to {item.duration} days</>}
                                  <br/>Base Cost: {item.cost}
